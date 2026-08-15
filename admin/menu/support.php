30a2148c7fe37e52c106070407fd9b26b81b149d
Your Name <godkiller_nt@hammermaps.de>
2026-08-15 20:11:28 +0200

    Expand admin support diagnostics

--- admin/menu/support.php
+++ admin/menu/support.php
@@ -6,64 +6,80 @@
 
 if (_adminMenu != 'true') exit;
 
-$PhpInfo = parsePHPInfo();
-$support = "#####################\r\n";
-$support .= "Support Informationen\r\n";
-$support .= "#####################\r\n";
+function supportState(bool $enabled): string
+{
+    return $enabled ? 'On' : 'Off';
+}
+
+function supportValue(string|false $value): string
+{
+    return $value === false || $value === '' ? '<nicht gesetzt>' : $value;
+}
+
+function supportSection(string $title): string
+{
+    return "#####################\r\n" . $title . "\r\n#####################\r\n";
+}
+
+$runtimeDirectories = array(
+    'Cache-Verzeichnis' => basePath . '/inc/_cache_',
+    'Log-Verzeichnis' => basePath . '/inc/_logs',
+    'Galerie-Uploads' => basePath . '/gallery/images',
+    'Taktik-Uploads' => basePath . '/inc/images/uploads/taktiken',
+);
+$extensions = array('mysqli', 'pdo_sqlite', 'imagick', 'gd', 'mbstring', 'intl', 'xml', 'bz2');
+$logFiles = glob(basePath . '/inc/_logs/*.{log,md,html}', GLOB_BRACE) ?: array();
+$latestLog = '';
+if ($logFiles) {
+    usort($logFiles, static fn(string $left, string $right): int => filemtime($right) <=> filemtime($left));
+    $latestLogTimestamp = filemtime($logFiles[0]) ?: 0;
+    $latestLog = basename($logFiles[0]) . ' (' . date('d.m.Y H:i', $latestLogTimestamp) . ')';
+}
+
+$support = supportSection('Support Informationen');
+$support .= 'Erstellt am: ' . date('d.m.Y H:i:s') . "\r\n";
+$support .= "Hinweis: Dieser Bericht enthält keine Zugangsdaten oder Log-Inhalte.\r\n\r\n";
+
+$support .= supportSection('DZCP Allgemein');
+$support .= 'DZCP Version: ' . _version . "\r\n";
+$support .= 'DZCP Release: ' . _release . "\r\n";
+$support .= 'DZCP Build: ' . _build . "\r\n";
+$support .= 'DZCP Template: ' . settings('tmpdir') . "\r\n";
+$support .= 'DZCP API-Version: ' . $api->getApiVersion() . "\r\n";
+$support .= 'API aktiviert: ' . supportState(api_enabled) . "\r\n";
+$support .= 'Domain: ' . str_replace('/admin', '', GetServerVars('HTTP_HOST')) . "\r\n\r\n";
+
+$support .= supportSection('Laufzeit und Datenbank');
+$support .= 'Server OS: ' . (function_exists('php_uname') ? php_uname() : '<nicht verfügbar>') . "\r\n";
+$support .= 'Webserver: ' . supportValue(GetServerVars('SERVER_SOFTWARE')) . "\r\n";
+$support .= 'PHP-Version: ' . PHP_VERSION . ' (' . PHP_SAPI . ")\r\n";
+$support .= 'PHP Speicherlimit: ' . supportValue(ini_get('memory_limit')) . "\r\n";
+$support .= 'PHP Zeitzone: ' . date_default_timezone_get() . "\r\n";
+$support .= 'Datenbanktreiber: ' . $mysql->driver() . "\r\n";
+$support .= 'Datenbank-Server Version: ' . db_server_info() . "\r\n";
+$support .= 'Persistente MySQLi-Verbindung: ' . supportState($mysql->driver() === 'mysql' && defined('mysqli_persistconns') && (bool)constant('mysqli_persistconns')) . "\r\n\r\n";
+
+$support .= supportSection('PHP Funktionen und Erweiterungen');
+$support .= 'fsockopen: ' . supportState(fsockopen_support()) . "\r\n";
+$support .= 'Sockets: ' . supportState(function_exists('socket_create')) . "\r\n";
+$support .= 'TrueType-Schriften: ' . supportState(function_exists('imagettftext')) . "\r\n";
+$support .= 'Datei-Uploads: ' . supportState((bool)ini_get('file_uploads')) . "\r\n";
+$support .= 'Upload-Maximum: ' . supportValue(ini_get('upload_max_filesize')) . "\r\n";
+$support .= 'POST-Maximum: ' . supportValue(ini_get('post_max_size')) . "\r\n";
+foreach ($extensions as $extension) {
+    $support .= 'Erweiterung ' . $extension . ': ' . supportState(extension_loaded($extension)) . "\r\n";
+}
 $support .= "\r\n";
 
-$support .= "#####################\r\n";
-$support .= "DZCP Allgemein \r\n";
-$support .= "#####################\r\n";
-$support .= "DZCP Version: " . _version . "\r\n";
-$support .= "DZCP Release: " . _release . "\r\n";
-$support .= "DZCP Build: " . _build . "\r\n";
-$support .= "DZCP Template: " . $tmpdir . "\r\n";
-$support .= "DZCP API-Version: " . $api->getApiVersion() . "\r\n";
-$support .= "Domain: " . str_replace('/admin', '', GetServerVars('HTTP_HOST')) . "\r\n";
-$support .= "\r\n";
-
-$support .= "#####################\r\n";
-$support .= "Server Versionen\r\n";
-$support .= "#####################\r\n";
-$support .= "Server OS: " . @php_uname() . "\r\n";
-$support .= "Webserver: " . (array_key_exists('apache2handler', $PhpInfo) ? (array_key_exists('Apache Version', $PhpInfo['apache2handler']) ? $PhpInfo['apache2handler']['Apache Version'] : 'PHP l&auml;uft als CGI <Keine Info>') : 'PHP l&auml;uft als CGI <Keine Info>') . "\r\n";
-$support .= "PHP-Version: " . phpversion() . " (" . php_sapi_type() . ")" . "\r\n";
-$support .= "Datenbank-Server Version: " . db_server_info() . "\r\n";
-$support .= "MySQLi-Persistente Datenbankverbindung: " . (mysqli_persistconns ? 'On' : 'Off') . "\r\n";
-$support .= "\r\n";
-
-$support .= "#####################\r\n";
-$support .= "Server Cache\r\n";
-$support .= "#####################\r\n";
-$support .= "PhpFastCache Version: " . Phpfastcache\Api::getVersion() . "\r\n";
-$support .= "Cache Storage: " . str_replace('\\Phpfastcache\\Drivers\\', '', $cache->getDriverName()) . "\r\n";
-$support .= "Cache Temporary Fallback: " . ($cache->getConfig()->isAutoTmpFallback() ? 'On' : 'Off') . "\r\n";
-$support .= "\r\n";
-
-$support .= "#####################\r\n";
-$support .= "Socket-Verbindungen \r\n";
-$support .= "#####################\r\n";
-$support .= "PHP fsockopen: " . (fsockopen_support() ? 'On' : 'Off') . "\r\n";
-$support .= "PHP Sockets: " . (function_exists("socket_create") && $PhpInfo['sockets']['Sockets Support'] == "enabled" ? 'On' : 'Off') . "\r\n";
-$support .= "\r\n";
-
-$support .= "#####################\r\n";
-$support .= "Servereinstellungen\r\n";
-$support .= "#####################\r\n";
-$support .= "open_basedir: " . $PhpInfo['Core']['open_basedir'][0] . "\r\n";
-$support .= "PHP-Memory Limit: " . $PhpInfo['Core']['memory_limit'][0] . "\r\n";
-$support .= "imagettftext(): " . (function_exists('imagettftext') == true ? 'existiert' : 'existiert nicht') . "\r\n";
-$support .= "file_uploads: " . $PhpInfo['Core']['file_uploads'][0] . "\r\n";
-$support .= "upload_max_filesize: " . $PhpInfo['Core']['upload_max_filesize'][0] . "\r\n";
-$support .= "sendmail_from: " . $PhpInfo['Core']['sendmail_from'][0] . "\r\n";
-$support .= "sendmail_path: " . $PhpInfo['Core']['sendmail_path'][0] . "\r\n";
-$support .= "\r\n";
-
-$support .= "#####################\r\n";
-$support .= "Debug\r\n";
-$support .= "#####################\r\n";
-$support .= "SQL-Error Datei: " . (file_exists(basePath . "/inc/_logs/sql_error_log.log") ? 'vorhanden' : 'keine') . "\r\n";
-$support .= "\r\n";
+$support .= supportSection('Cache, Schreibrechte und Protokollierung');
+$support .= 'PhpFastCache Version: ' . Phpfastcache\Api::getVersion() . "\r\n";
+$support .= 'Cache Storage: ' . str_replace('\\Phpfastcache\\Drivers\\', '', $cache->getDriverName()) . "\r\n";
+$support .= 'Cache Temporary Fallback: ' . supportState($cache->getConfig()->isAutoTmpFallback()) . "\r\n";
+foreach ($runtimeDirectories as $label => $path) {
+    $support .= $label . ': ' . (is_dir($path) && is_writable($path) ? 'beschreibbar' : 'nicht beschreibbar') . "\r\n";
+}
+$support .= 'Letzte Protokolldatei: ' . ($latestLog ?: 'keine') . "\r\n";
+$support .= 'Entwicklungs-Fehleranzeige: ' . supportState(view_error_reporting) . "\r\n";
+$support .= 'SQL-Error Datei: ' . (file_exists(basePath . '/inc/_logs/sql_error_log.log') ? 'vorhanden' : 'keine') . "\r\n";
 
 $show = show($dir . "/support", array("info" => _admin_support_info, "head" => _admin_support_head, "support" => $support));
